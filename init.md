cca492340fd051066561a7d8f36bb56e14162a16
Lily Song <lilysong@cisco.com>
2023-08-16 17:18:17 +0800

    jisuanshuxing

---
+++ init.md
@@ -0,0 +1,8 @@
+创建一个Vue应用
+
+```bash
+npm init vue@latest
+
+```
+
+这一指令将会安装并执行 [create-vue](https://github.com/vuejs/create-vue)，它是 Vue 官方的项目脚手架工具。
--- rejoice-20230720/src/views/HomeView.vue
+++ rejoice-20230720/src/views/HomeView.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
+  import { ref, reactive, computed } from 'vue';
   const count = ref(0);
   function increment() {
     count.value++;
   } 
+
+  const author = reactive({
+    name: 'John Doe',
+    books: [
+      'Vue 2 - Advanced Guide',
+      'Vue 3 - Basic Guide',
+      'Vue 4 - The Mystery'
+    ]
+  })
+
+  const publishedBooksNumber = computed(() => {
+    return author.books.length
+  })
+
+  const firstName = ref('lily');
+  const lastName = ref('song');
+  let fullName = computed({
+    get() {
+      return firstName.value + lastName.value;
+    },
+    set(newValue) {
+      // 注意：我们这里使用的是解构赋值语法
+      [firstName.value, lastName.value] = newValue.split(' ')
+    }
+  });
+
+  fullName.value = 'William Tong';
 </script>
 
 <template>
   <main>
     <span>{{ count }}</span>
     <button @click = increment()>click</button>
+    <br>
+    <p>has published books: {{ publishedBooksNumber }} books</p>
+    <p>{{firstName + lastName  + fullName }}</p>
   </main>
 </template>
