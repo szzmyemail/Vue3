b5b722c6f28067c6d6a6ba291dd38a66203e46de
Lily Song <lilysong@cisco.com>
2023-08-22 16:20:19 +0800

    watch

--- rejoice-20230720/src/App.vue
+++ rejoice-20230720/src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      
     </div>
   </header>
 
--- rejoice-20230720/src/views/AboutView.vue
+++ rejoice-20230720/src/views/AboutView.vue
@@ -1,15 +1,29 @@
+<script setup lang="ts">
+  import { onUpdated, ref } from 'vue';
+  import WatchLearn from '@/components/WatchLearn.vue';
+  let todoList = ref([1, 2, 3]);
+  //如何给一个响应式属性重新赋值？？？？
+  function changeArr() {
+    todoList.value = [3, 2, 1];;
+  }
+
+  onUpdated(() =>{
+    console.log('DOM 更新发生了');
+  })
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click = changeArr()>click</button>
+    <ul>
+      <li v-for="(val, i) of todoList" :class="i + 'a'" :key="i">{{ val }}</li>
+    </ul>
+    <h3>watch</h3>
+    <WatchLearn/>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+ 
 }
 </style>
