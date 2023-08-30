fb65526b02a95c1beeb69b83ea543491e1e744fc
Lily Song <lilysong@cisco.com>
2023-08-30 17:49:29 +0800

    life cycle

---
+++ rejoice-20230720/src/components/LifeCycle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import { onMounted } from 'vue';
+    onMounted(() => {
+        console.log(`the component is now mounted.`)
+    })
+    
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>生命周期钩子函数</h3>
+  </div>
+</template>
+
+<style scoped>
+ h3 {
+    color: pink;
+ }
+</style>
